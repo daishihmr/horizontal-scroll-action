4e5d270b71816efdba4d9b78619f5c6015e2b084
daishihmr <daishi.hmr@gmail.com>
2013-12-11 17:15:37 +0900

    3

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+var app;
+
 tm.main(function() {
-    console.log("ok");
+    console.log("main start");
+
+    app = tm.display.CanvasApp("#app");
+    app.resize(320, 320).fitWindow();
+    app.replaceScene(tm.app.LoadingScene({
+        width: 320,
+        height: 320,
+        assets: {
+        },
+        nextScene: MainScene
+    }));
+    app.run();
+});
+
+tm.define("MainScene", {
+    superClass: tm.app.Scene,
+
+    init: function() {
+        this.superInit();
+
+        console.log("start MainScene");
+    }
+
 });
